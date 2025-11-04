95a26ee849d991390dc5418b538de5ad09ecf1fa
Aman Maseeh Luqman KA <llukku879@gmail.com>
2025-11-04 10:37:30 +0530

    Add files via upload

---
+++ over.py
@@ -0,0 +1,9 @@
+n=int(input("total number of integers:"))
+list=[]
+for i in range(n):
+    a=int(input("enter an integer:"))
+    ifa<100:
+        list1.append(a)
+        else:
+            list1.append("over")
+   print(list1)         
